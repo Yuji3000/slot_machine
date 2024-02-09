21e946ebce32c7dd4c4925b161c64e10e10a318d
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-08 20:57:02 -0700

    done

--- main.py
+++ main.py
@@ -15,7 +15,35 @@ symbol_count = {
 }
 
 def get_slot_machine_spin(rows, cols, symbols):
-    
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        print()
+
+
 
 def deposit():
     while True:
@@ -70,5 +98,7 @@ def main():
         
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
     # print(balance, lines, bet)
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
